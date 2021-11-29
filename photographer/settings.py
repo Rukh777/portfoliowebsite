a328ff08c3761c12b554a6cefbb49c247446ce85
Rukhsar Khan <rukhsarmev07@gmail.com>
2021-11-29 19:08:16 +0530

    deploy

--- photographer/settings.py
+++ photographer/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = '/static/'
+# STATIC_ROOT = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 #media files
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
